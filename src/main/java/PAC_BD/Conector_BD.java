61a560e802572f55936d3e179aa73ab5573d8644
RoyZamudio <royzamudiotovar@gmail.com>
2025-04-25 13:12:31 -0600

    Conexión a BD

---
+++ src/main/java/PAC_BD/Conector_BD.java
@@ -0,0 +1,21 @@
+package PAC_BD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conector_BD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/SoundSphere?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String USER = "root";
+    private static final String PASSWORD = "1234"; //
+
+    public static Connection conectar() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("No se pudo cargar el driver de MySQL", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
